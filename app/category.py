20422adcd1193518095bf8e45b0621f13963ac61
HugoRoca <hugo.rock20@hotmail.com>
2022-12-14 08:59:38 -0500

    Create tablr from python code

--- app/category.py
+++ app/category.py
@@ -4,7 +4,24 @@ from flask_marshmallow import Marshmallow
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost:3306/dbpythonapi'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(200))
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        
+
+with app.app_context():        
+    db.create_all()
 
 # welcome message
 @app.route('/', methods=['GET'])
